afb65aa51a46cccf2ad095b04369734b985e9828
rharris529 <jordandine@gmail.com>
2022-02-03 01:24:06 -0600

    created timeline for tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 var currentDay = document.querySelector("currentDay");
-var schedule = document.querySelector(".container");
+var task = document.getElementById("saveBtn");
 
 // using moment.js, display time dynamically
 var now = moment().format('LL');
@@ -7,46 +7,8 @@ var now = moment().format('LL');
 // Will display current day when page loads
 $('#currentDay').html(now);
 
-// Display an 9am - 5pm grid
-var displayHours = function() {
-    var time = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+// Function to save task to local storage
+var save = function() {
 
-    for (var i = 0; i = time.length; i++) {
-
-        var grid = document.createElement("p");
-        var put = grid.innerText = i + "pm";
-        
-        schedule.appendChild(put);
-    }
-
-    console.log(put);
 }
 
-var timeBlocks = function() {
-
-    for (var i = 0; i < 8; i++) {
-
-
-        // create submit field
-        var task = document.createElement("input");
-        task.className = "input-group-append ";
-
-        // create a save button
-        var save = document.createElement("button");
-        save.className = "btn-primary saveBtn";
-
-        // display the time
-        var hours = moment().format("ha");
-        console.log(hours);
-        
-        
-        schedule.appendChild(task);
-        schedule.appendChild(save);
-        
-    }
-    
-    
-}
-
-displayHours();
-timeBlocks();
